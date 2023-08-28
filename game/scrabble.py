c4d7c1d36f4d2446e7759fdb34e10185bbb3c97f
Martin-Navarro-T <mt.navarro@alumno.um.edu.ar>
2023-08-27 21:49:02 -0300

    Tercer Commit

---
+++ game/scrabble.py
@@ -0,0 +1,13 @@
+from game.board import Board
+from game.player import Player
+from game.models import BagTiles
+
+
+class ScrabbleGame:
+    def __init__(self, players_count):
+        self.board = Board()
+        self.bag_tiles = BagTiles()
+        self.players = []
+        for _ in range(players_count):
+            self.players.append(Player())
+           
