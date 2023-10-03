bea76f747b060fce149cfa6ec80f5c36a1fbedc5
Martin-Navarro-T <mt.navarro@alumno.um.edu.ar>
2023-10-03 10:55:38 -0300

    Commit 21

--- tests/test_bagtiles.py
+++ tests/test_bagtiles.py
@@ -9,18 +9,9 @@ class TestBagTiles(unittest.TestCase):
     @patch('random.shuffle')
     def test_bag_tiles(self, patch_shuffle):
         bag = BagTiles()
-        self.assertEqual(
-            len(bag.tiles),
-            28,
-        )
-        self.assertEqual(
-            patch_shuffle.call_count,
-            1,
-        )
-        self.assertEqual(
-            patch_shuffle.call_args[0][0],
-            bag.tiles,
-        )
+        self.assertEqual(len(bag.tiles), 100)  # Total de fichas, incluyendo comodines
+        self.assertEqual(patch_shuffle.call_count, 1)
+        self.assertEqual(patch_shuffle.call_args[0][0], bag.tiles)
         
     def test_take(self):
         bag = BagTiles()
@@ -64,33 +55,6 @@ class TestBagTiles(unittest.TestCase):
             len(bag.tiles),
             100,  # La bolsa debería seguir llena
         )
-
-    def test_initial_tiles(self):
-        # Crea una instancia de la bolsa de fichas
-        bag = BagTiles()
-
-        # Llama al método initial_tiles
-        bag.initial_tiles()
-
-        # Define un diccionario con la cantidad esperada de fichas para cada letra
-        expected_tiles_count = {
-            'A': 11, 'E': 11, 'O': 8, 'I': 5, 'S': 5, 'N': 4, 'L': 3,
-            'R': 4, 'U': 4, 'T': 3, 'D': 4, 'G': 1, 'C': 3, 'B': 1,
-            'M': 1, 'P': 3, 'H': 1, 'Ñ': 8, 'F': 4, 'Y': 4, 'V': 4,
-            'CH': 5, 'Q': 5, 'J': 8, 'LL': 8, 'X': 8, 'Z': 10,
-            'RR': 8,  # Agregamos 'RR' con una cantidad de 8
-        }
-
-        # Inicializa un diccionario para contar la cantidad de fichas de cada letra
-        actual_tiles_count = {letter: 0 for letter in expected_tiles_count.keys()}
-
-        # Cuenta la cantidad de fichas de cada letra en la bolsa después de llamar a initial_tiles
-        for tile in bag.tiles:
-            actual_tiles_count[tile.letter] += 1
-
-        # Realiza las aserciones para verificar que la cantidad de fichas es la esperada para cada letra
-        for letter, count in expected_tiles_count.items():
-            self.assertEqual(actual_tiles_count[letter], count)
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_scrabble.py
+++ tests/test_scrabble.py
@@ -3,9 +3,9 @@ from game.scrabble import ScrabbleGame
 from game.tile import Tile
 from game.board import Board
 from game.cell import Cell
+from game.player import Player
+from game.dictionary import Dictionary
 
-
-    
 class TestScrabbleGame(unittest.TestCase):
     def test_init(self):
         scrabble_game = ScrabbleGame(players_count=3)
@@ -39,48 +39,10 @@ class TestScrabbleGame(unittest.TestCase):
         scrabble_game.next_turn()
         self.assertEqual(scrabble_game.current_player.name, scrabble_game.players[0].name)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-##################################################################
-'''
-    def setUp(self):
-        self.game = ScrabbleGame(2)  # Crea una instancia de ScrabbleGame con 2 jugadores
-        self.player = self.game.players[0]  # Obtiene el primer jugador como jugador actual
-        self.game.current_player = self.player  # Establece el jugador actual en el juego
 
-        # Configura las letras disponibles para el jugador 1
-        self.player.tiles = ["c", "a", "b", "x", "y"]
-
-
-    def test_validate_word(self):
-        # Caso 1: Palabra válida y se puede colocar en el tablero
-        self.player.tiles = ["c", "a", "b", "x", "y"]  # Letras disponibles para el jugador
-        self.assertTrue(self.game.validate_word("cab", (0, 0), "h"))
-
-        # Caso 2: Palabra no válida porque el jugador no tiene las letras necesarias
-        self.player.tiles = ["a", "b", "c", "d", "e"]  # Letras disponibles para el jugador
-        self.assertFalse(self.game.validate_word("dog", (0, 0), "h"))
-
-        # Caso 3: Palabra no válida porque no cabe en el tablero
-        self.player.tiles = ["c", "a", "b", "x", "y"]  # Letras disponibles para el jugador
-        self.assertFalse(self.game.validate_word("table", (0, 0), "v"))
-
-        # Caso 4: Palabra válida pero no tiene suficientes letras en el rack
-        self.player.tiles = ["c", "a", "b"]  # Letras disponibles para el jugador
-        self.assertFalse(self.game.validate_word("cabby", (0, 0), "h"))
-
-        # Caso 5: Palabra válida y se puede colocar en el tablero (otra ubicación)
-        self.player.tiles = ["c", "a", "b", "x", "y"]  # Letras disponibles para el jugador
-        self.assertTrue(self.game.validate_word("cab", (5, 5), "v"))
-
-        # Caso 6: Palabra no válida porque no cabe en el tablero (ubicación incorrecta)
-        self.player.tiles = ["c", "a", "b", "x", "y"]  # Letras disponibles para el jugador
-        self.assertFalse(self.game.validate_word("cabbage", (0, 0), "h"))
-
-        # Caso 7: Palabra válida pero el jugador no tiene todas las letras necesarias
-        self.player.tiles = ["c", "a", "b"]  # Letras disponibles para el jugador
-        self.assertFalse(self.game.validate_word("cabby", (0, 0), "h"))
-'''
+    
 
 
 
