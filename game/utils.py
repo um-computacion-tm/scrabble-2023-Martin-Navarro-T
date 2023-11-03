9bd965a181a5d25a034a33ad51a393df6f72e896
Martin-Navarro-T <mt.navarro@alumno.um.edu.ar>
2023-11-03 12:07:34 -0300

    Commit 33

--- game/utils.py
+++ game/utils.py
@@ -23,3 +23,42 @@ class Utils:
         elif orientation == "Vertical":
             row += 1
         return row, column
+    
+    def word_to_tiles(self, word):
+        tiles_list = []
+        i = 0
+        while i < len(word):
+            two_letter_combo = word[i:i+2]
+            if two_letter_combo.upper() in ('CH', 'LL', 'RR'):
+                self.convert_special_to_tiles(two_letter_combo, tiles_list)
+                i += 2
+            else:
+                self.convert_string_to_tiles(word[i], tiles_list)
+                i += 1
+        return tiles_list
+    
+    def compare_tiles_and_letters(self, tile, letter):
+        if tile is not None:
+            if tile.letter == letter.upper():
+                return 1
+            else:
+                return 0
+        else:
+            return
+    
+    def remove_duplicate_columns(self, list_tuples):
+        columns = {}
+        for location_x, location_y in list_tuples:
+            if location_y not in columns:
+                columns[location_y] = (location_x, location_y)
+        list_tuples = list(columns.values())
+        return list_tuples
+    
+    def remove_duplicate_rows(self, list_tuples):
+        rows = {}
+        for location_x, location_y in list_tuples:
+            if location_x not in rows:
+                rows[location_x] = (location_x, location_y)
+        list_tuples = list(rows.values())
+        return list_tuples
+     
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -17,5 +17,100 @@ class TestTuClase(unittest.TestCase):
         self.assertEqual(row, 2)
         self.assertEqual(column, 2)
 
+    # word_to_tiles
+    def test_word_to_tiles_simple_hola(self):
+        utils = Utils() 
+        list_tiles = utils.word_to_tiles("hola")
+        self.assertEqual(list_tiles[0].letter, "H")
+        self.assertEqual(list_tiles[0].value, 4)
+        self.assertEqual(list_tiles[1].letter, "O")
+        self.assertEqual(list_tiles[1].value, 1)
+        self.assertEqual(list_tiles[2].letter, "L")
+        self.assertEqual(list_tiles[2].value, 1)
+        self.assertEqual(list_tiles[3].letter, "A")
+        self.assertEqual(list_tiles[3].value, 1)
+        
+    def test_word_to_tiles_simple_facultad(self):
+        utils = Utils() 
+        list_tiles = utils.word_to_tiles("facultad")
+        self.assertEqual(list_tiles[0].letter, "F")
+        self.assertEqual(list_tiles[0].value, 4)
+        self.assertEqual(list_tiles[1].letter, "A")
+        self.assertEqual(list_tiles[1].value, 1)
+        self.assertEqual(list_tiles[2].letter, "C")
+        self.assertEqual(list_tiles[2].value, 2)
+        self.assertEqual(list_tiles[3].letter, "U")
+        self.assertEqual(list_tiles[3].value, 1)
+        self.assertEqual(list_tiles[4].letter, "L")
+        self.assertEqual(list_tiles[4].value, 1)
+        self.assertEqual(list_tiles[5].letter, "T")
+        self.assertEqual(list_tiles[5].value, 1)
+        self.assertEqual(list_tiles[6].letter, "A")
+        self.assertEqual(list_tiles[6].value, 1)
+        self.assertEqual(list_tiles[7].letter, "D")
+        self.assertEqual(list_tiles[7].value, 2)
+        
+    def test_word_to_tiles_simple_casa(self):
+        utils = Utils() 
+        list_tiles = utils.word_to_tiles("casa")
+        self.assertEqual(list_tiles[0].letter, "C")
+        self.assertEqual(list_tiles[0].value, 2)
+        self.assertEqual(list_tiles[1].letter, "A")
+        self.assertEqual(list_tiles[1].value, 1)
+        self.assertEqual(list_tiles[2].letter, "S")
+        self.assertEqual(list_tiles[2].value, 1)
+        self.assertEqual(list_tiles[3].letter, "A")
+        self.assertEqual(list_tiles[3].value, 1)
+        
+    def test_word_to_tiles_complex_CH(self):
+        utils = Utils() 
+        list_tiles = utils.word_to_tiles("chita")
+        self.assertEqual(list_tiles[0].letter, "CH")
+        self.assertEqual(list_tiles[0].value, 5)
+        self.assertEqual(list_tiles[1].letter, "I")
+        self.assertEqual(list_tiles[1].value, 1)
+        self.assertEqual(list_tiles[2].letter, "T")
+        self.assertEqual(list_tiles[2].value, 1)
+        self.assertEqual(list_tiles[3].letter, "A")
+        self.assertEqual(list_tiles[3].value, 1)
+        
+    def test_word_to_tiles_complex_RR(self):
+        utils = Utils() 
+        list_tiles = utils.word_to_tiles("perro")
+        self.assertEqual(list_tiles[0].letter, "P")
+        self.assertEqual(list_tiles[0].value, 2)
+        self.assertEqual(list_tiles[1].letter, "E")
+        self.assertEqual(list_tiles[1].value, 1)
+        self.assertEqual(list_tiles[2].letter, "RR")
+        self.assertEqual(list_tiles[2].value, 8)
+        self.assertEqual(list_tiles[3].letter, "O")
+        self.assertEqual(list_tiles[3].value, 1)
+        
+    def test_word_to_tilescomplex_LL(self):
+        utils = Utils() 
+        list_tiles = utils.word_to_tiles("llanto")
+        self.assertEqual(list_tiles[0].letter, "LL")
+        self.assertEqual(list_tiles[0].value, 8)
+        self.assertEqual(list_tiles[1].letter, "A")
+        self.assertEqual(list_tiles[1].value, 1)
+        self.assertEqual(list_tiles[2].letter, "N")
+        self.assertEqual(list_tiles[2].value, 1)
+        self.assertEqual(list_tiles[3].letter, "T")
+        self.assertEqual(list_tiles[3].value, 1)
+        self.assertEqual(list_tiles[4].letter, "O")
+        self.assertEqual(list_tiles[4].value, 1)
+
+    def test_remove_duplicate_columns(self):
+        utils = Utils()
+        list = [(7,7), (9,7)]
+        list = utils.remove_duplicate_columns(list)
+        self.assertEqual(list, [(7,7)])
+
+    def test_remove_duplicate_rows(self):
+        utils = Utils()
+        list = [(7,7), (7,9)]
+        list = utils.remove_duplicate_rows(list)
+        self.assertEqual(list, [(7,7)])
+        
 if __name__ == '__main__':
     unittest.main()
